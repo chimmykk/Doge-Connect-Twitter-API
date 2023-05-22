d21cdb183025bebdf07722fbca816933f6c3bcc3
Yeite Rilsosing Koireng <53212931+chimmykk@users.noreply.github.com>
2023-05-22 20:22:38 +0530

    test

--- index.js
+++ index.js
@@ -7,11 +7,9 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 
 dotenv.config();
-
 app.use(cors());
 
 const TOKEN ='AAAAAAAAAAAAAAAAAAAAAHJ9nAEAAAAAomau5hMP8%2Fus0oP2sfkqReBJTmw%3DUGiJFDfts0IQh55KvVDxsnKUEo1uibQ4aQyslLeUKPjvp94pL2';
-
 const streamURL =
   'https://api.twitter.com/2/tweets/search/stream?tweet.fields=public_metrics,created_at&expansions=author_id&user.fields=username,name,profile_image_url';
   
@@ -23,11 +21,9 @@ async function getAllRules() {
       Authorization: `Bearer ${TOKEN}`,
     },
   });
-
   if (response.statusCode !== 200) {
     throw new Error(response.body);
   }
-
   return response.body;
 }
 
@@ -35,34 +31,28 @@ async function deleteAllRules(rules) {
   if (!Array.isArray(rules.data)) {
     return null;
   }
-
   const ids = rules.data.map((rule) => rule.id);
-  
   const data = {
     delete: {
       ids: ids,
     },
   };
-
   const response = await needle('post', rulesURL, data, {
     headers: {
       'content-type': 'application/json',
       Authorization: `Bearer ${TOKEN}`,
     },
   });
-
   if (response.statusCode !== 200) {
     throw new Error(response.body);
   }
-
   return response.body;
 }
 
 async function setRules(rules) {
   const data = {
     add: rules,
-  };
-
+  }
   const response = await needle('post', rulesURL, data, {
     headers: {
       'content-type': 'application/json',
@@ -70,11 +60,9 @@ async function setRules(rules) {
     },
     json: true
   });
-
   if (response.statusCode !== 201) {
     throw new Error(JSON.stringify(response.body));
   }
-
   return response.body;
 }
 
@@ -91,19 +79,18 @@ stream.on('data', async (data) => {
     const json = JSON.parse(data);
     const tweetText = json.data.text;
     const tweetId = json.data.id;
-    
     console.log(`Tweet ID: ${tweetId}`);
     console.log(`Tweet Text: ${tweetText}`);
     
     await checkMediaUrl(json, tweetId);
-    
+
     try {
       const apiUrl = `https://api.twitter.com/2/tweets?ids=${tweetId}&expansions=attachments.media_keys&media.fields=media_key,type,url,preview_image_url`;
       
       const response = await needle('get', apiUrl, {
         headers: { Authorization: `Bearer ${TOKEN}` },
       });
-      
+
       if (
         response.statusCode === 200 &&
         response.body.data[0].attachments &&
@@ -112,7 +99,6 @@ stream.on('data', async (data) => {
         const mediaObj = response.body.includes.media.find(
           (media) => media.type === 'photo' || media.type === 'animated_gif'
         );
-        
         if (mediaObj && mediaObj.url) {
           console.log('Media URL found:', mediaObj.url);
           json.media_url = mediaObj.url;
@@ -120,13 +106,9 @@ stream.on('data', async (data) => {
       }
 
       tweetCount++;
-      
       const filename = `${tweetCount}.json`;
-
       const filePath = path.join(__dirname, 'pulltweets', filename);
-      
       fs.writeFileSync(filePath, JSON.stringify(json, null, 2));
-      
     } catch (error) {
       console.error(`Error writing JSON file: ${error}`);
     }
@@ -138,7 +120,6 @@ stream.on('data', async (data) => {
 async function checkMediaUrl(json, tweetId) {
   try {
     const apiUrl = `https://api.twitter.com/2/tweets?ids=${tweetId}&expansions=attachments.media_keys&media.fields=media_key,type,url,preview_image_url`;
-    
     const response = await needle('get', apiUrl, {
       headers: { Authorization: `Bearer ${TOKEN}` },
     });
@@ -151,64 +132,77 @@ async function checkMediaUrl(json, tweetId) {
       const mediaObj = response.body.includes.media.find(
         (media) => media.type === 'photo' || media.type === 'animated_gif'
       );
-
       if (mediaObj && mediaObj.url) {
         console.log('Media URL found:', mediaObj.url);
-        
-        json.media_url = mediaObj.url;
-        
+        json.media_url = mediaObj.url; 
       } else {
         console.log('No media URL found.');
-        
-        json.media_url = '';
-        
+        json.media_url = ''; 
       }
-      
     } else {
-      
        json.media_url = '';
-       
        console.log('No media URL found.');
-       
     }
-    
   } catch (error) {
     console.error(`Error checking media URL: ${error}`);
   }
+}
+
+async function updateMetrics() {
+  const pullTweetsDir = path.join(__dirname, 'pulltweets');
+  const files = fs.readdirSync(pullTweetsDir);
+
+  files.forEach(async (file) => {
+    const filePath = path.join(pullTweetsDir, file);
+    const jsonData = fs.readFileSync(filePath);
+    const tweetData = JSON.parse(jsonData);
+
+    try {
+      const apiUrl = `https://api.twitter.com/2/tweets/${tweetData.data.id}?tweet.fields=public_metrics`;
+      const response = await needle('get', apiUrl, {
+        headers: { Authorization: `Bearer ${TOKEN}` },
+      });
+
+      if (response.statusCode === 200 && response.body.data) {
+        console.log('Updated metrics for tweet:', tweetData.data.id);
+        tweetData.data.public_metrics = response.body.data.public_metrics;
+        fs.writeFileSync(filePath, JSON.stringify(tweetData));
+      }
+    } catch (error) {
+      console.error(`Error updating metrics for tweet ${tweetData.data.id}: ${error}`);
+    }
+  });
 }
 
 async function main() {
   let currentRules;
 
   try {
-
     currentRules = await getAllRules();
-    
     await deleteAllRules(currentRules);
 
     const usernamesJson = fs.readFileSync(path.join(__dirname, 'rules.json'));
-    
     const usernamesData = JSON.parse(usernamesJson);
-    
     const usernamesRules = usernamesData.usernames.map(username => ({ value: `from:${username}` }));
-    
+
     const dogePoundRules = [
       { value: 'Dogepound -is:retweet -is:reply' },
-      { value: 'Doge Pound -is:retweet -is:reply' },
-      { value: 'The Doge Pound -is:retweet -is:reply' },
+      { value: 'DogePound -is:retweet -is:reply' },
+      { value: 'TheDogePound -is:retweet -is:reply' },
       { value: '@thedogepoundnft -is:retweet -is:reply' },
       { value: 'TheDogePound -is:retweet -is:reply' },
-      {value: 'Doge -is: retweet -is:reply'}
     ];
-    
+
     await setRules([...usernamesRules, ...dogePoundRules]);
   } catch (error) {
     console.error(error);
     process.exit(1);
   }
-  
+
   app.use('/pulltweets', express.static(path.join(__dirname, 'pulltweets')));
-  
+
+  setInterval(updateMetrics, 60 * 1000); // Run every minute
+
   app.listen(8080, () => {
     console.log('Server started on port 8080');
   });
